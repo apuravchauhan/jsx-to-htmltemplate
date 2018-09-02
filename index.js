9ad601104818f254d5ceabdb4185df4891a8bf9e
Apurav Chauhan <apurav.chauhan@gmail.com>
2018-09-02 13:46:59 +0530

    Updated with test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /**
- * @author: Apurav Chauhan<apurav.chauhan@gmail.com | github/twitter: apuravchauhan>
+ * @author: Apurav Chauhan | twitter: apuravchauhan
  */
 const chalk = require('chalk');
 const figlet = require('figlet');
@@ -11,12 +11,13 @@ const posthtml = require('posthtml');
 const CLI = require('clui');
 const fs = require('fs');
 const Spinner = CLI.Spinner;
-var posthtmlFreemarker = require('posthtml-jsxhtml-freemarker');
+const pretty = require('pretty');
+const posthtmlFreemarker = require('posthtml-jsxhtml-freemarker');
 
 //const argv = require('minimist')(process.argv.slice(2));
 console.log(
   chalk.yellow(
-    figlet.textSync('jsx-htmltemplate', {horizontalLayout: 'default' })
+    figlet.textSync('jsx-htmltemplate', { horizontalLayout: 'default' })
   )
 );
 
@@ -50,40 +51,46 @@ try {
   fileUtil.mkdirSyncRecursive(outputPath);
 
   fs.readdirSync(basePath).map((caseName) => {
-    var transformed='',dest='';
-    if (caseName.endsWith('.js')){
+    var transformed = '', dest = '';
+    if (caseName.endsWith('.js')) {
       transformed = babel.transformFileSync(
-        path.join(basePath, caseName),{
+        path.join(basePath, caseName), {
           presets: ["es2015"],
           plugins: ["jsx-to-generichtml", "syntax-jsx",
             "syntax-class-properties"]
         }
       ).code;
-      dest = path.join(outputPath,caseName);
-    } else{
-       transformed = babel.transformFileSync(
-        path.join(basePath,caseName, "index.js"),{
+      dest = path.join(outputPath, caseName);
+    } else {
+      transformed = babel.transformFileSync(
+        path.join(basePath, caseName, "index.js"), {
           presets: ["es2015"],
           plugins: ["jsx-to-generichtml", "syntax-jsx",
             "syntax-class-properties"]
         }
       ).code;
-      dest = path.join(outputPath,caseName,"index.js");
-      fileUtil.mkdirSyncRecursive(path.join(outputPath,caseName));
+      dest = path.join(outputPath, caseName, "index.js");
+      fileUtil.mkdirSyncRecursive(path.join(outputPath, caseName));
     }
-    if(transformed.endsWith(';')){
-      transformed =  transformed.slice(0,transformed.length-1);
+    if (transformed.endsWith(';')) {
+      transformed = transformed.slice(0, transformed.length - 1);
     }
-    if(transformed.startsWith("'use strict';") || transformed.startsWith("\"use strict\";")){
-      transformed =  transformed.slice(13,transformed.length);
+    if (transformed.startsWith("'use strict';") || transformed.startsWith("\"use strict\";")) {
+      transformed = transformed.slice(13, transformed.length);
     }
     fs.writeFileSync(dest, transformed);
   });
 
-  var contents = fs.readFileSync(path.join(outputPath,entryFile), 'utf8');
+  var contents = fs.readFileSync(path.join(outputPath, entryFile), 'utf8');
   var output = posthtml([posthtmlFreemarker({ loc: outputPath })])
-    .process(contents, { sync: true })
-  console.log(output.html);
+    // .use(beautify({rules: {indent: 2}}))
+    .process(contents, { sync: true });
+  console.log(`
+      
+    ${pretty(output.html, { ocd: true })}
+  
+  `)
+
 } finally {
   status.stop();
 }
---
+++ test/comp1/index.js
@@ -0,0 +1,1 @@
+<p>Child component loaded</p>
